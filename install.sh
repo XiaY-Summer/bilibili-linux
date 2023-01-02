f38006026dcfb9cf266f76eef1d7b7ae0d672663
XiaY-Summer <107897606+XiaY-Summer@users.noreply.github.com>
2023-01-02 14:00:31 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+
 curl -o bilibili.tar.gz http://8.219.163.22:8888/down/PW3itT8N8R3w
 tar xf bilibili.tar.gz
-cd bilibili-linux
-chmod +x install.sh uninstall.sh
-sudo ./install.sh
+
+sudo cp -r bilibili-linux /opt
+
+sudo cp -r bilibili.desktop /usr/share/applications
+
+echo "安装完成"
