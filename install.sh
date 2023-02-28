1a92de29643b550241dfbf6f8c6b6925ae6e64aa
XiaY-Summer <107897606+XiaY-Summer@users.noreply.github.com>
2023-02-28 19:32:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -o bilibili.tar.gz https://192.168.31.44:38368/down/qOjw8erht24E.gz
+curl -o bilibili.tar.gz http://192.168.31.44:9999/?f=/%E5%A4%87%E4%BB%BD/%E5%93%94%E5%93%A9%E5%93%94%E5%93%A9Linux/bilibili.tar.gz
 tar xf bilibili.tar.gz
 
 sudo cp -r bilibili-linux /opt
